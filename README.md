2329809ab79dc08ac417ed0871136b413cb40d7d
shinjinseop <yeo11200@hanmail.net>
2023-12-09 13:12:16 +0900

    [main] first webhooks

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# webhooks-simpleing
-Simple webhooks for Slack and JANDI
+# 잔디와 슬랙을 간단하게 웹 훅으로 사용하기 위해서 만든 라이브러리
+
+## 사용설명서
+
+1. 함수 인자에 대한 type
+
+```typescript
+  // jandi인지 slack인지 여부
+  type: "jandi" | "slack";
+
+  // webhooks의 url을 입력
+  urlType: string[];
+
+  // 응답에 대한 제목
+  title: string;
+
+  // 응답에 대한 내용
+  content: string;
+
+  // 응답에 대한 callbakc으로 필요한지 여부
+  callback?: (res: Response) => {};
+```
+
+2. 사용 설명서
+
+```javascript
+webhooksSendMessage({
+  type: "jandi",
+  urlType: ["https://wh.jandi.com/connect-api/webhook/<webhooks>"],
+  title: "message",
+  content: "submessage",
+});
+```
---
+++ index.ts
@@ -0,0 +1,86 @@
+import fetch from "node-fetch";
+
+export type Response = {
+  status?: number;
+  statusText?: string;
+  errorCode?: string;
+};
+
+export interface WebhooksSendMessage {
+  // jandi인지 slack인지 여부
+  type: "jandi" | "slack";
+
+  // webhooks의 url을 입력
+  urlType: string[];
+
+  // 응답에 대한 제목
+  title: string;
+
+  // 응답에 대한 내용
+  content: string;
+
+  // 응답에 대한 callbakc으로 필요한지 여부
+  callback?: (res: Response) => {};
+}
+
+const webhooksSendMessage = async ({
+  type,
+  urlType,
+  title,
+  content,
+  callback,
+}: WebhooksSendMessage) => {
+  const isJandi = type === "jandi";
+  const url: string[] = urlType;
+  const urlLength: number = url.length;
+
+  let headers: { "Content-type": string; Accept?: string } = {
+    "Content-type": "application/json",
+  };
+
+  let payload: string = "";
+
+  if (isJandi) {
+    headers = {
+      ...headers,
+      ...{ Accept: "application/vnd.tosslab.jandi-v2+json" },
+    };
+
+    payload = JSON.stringify({
+      body: title,
+      connectColor: "#00C473",
+      connectInfo: [{ description: content }],
+    });
+  } else {
+    payload = JSON.stringify({
+      text: title,
+      attachments: [
+        {
+          text: content,
+          fallback: "You are unable to choose a game",
+          callback_id: "wopr_game",
+          color: "#3AA3E3",
+          attachment_type: "default",
+        },
+      ],
+    });
+  }
+
+  for (let i = 0; i < urlLength; i++) {
+    await fetch(url[i], {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers,
+      body: payload,
+    })
+      .then((res) => {
+        callback && callback(res);
+      })
+      .catch((error) => {
+        callback && callback(error);
+      });
+  }
+};
+
+export default {
+  webhooksSendMessage,
+};
